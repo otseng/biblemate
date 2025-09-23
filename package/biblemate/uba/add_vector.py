0f5ace7bab13249a722d3b274a243f2d297d7e90
eliranwong <hkwebtech@gmail.com>
2025-09-23 21:11:52 +0100

    updated add vector script

--- package/biblemate/uba/add_vector.py
+++ package/biblemate/uba/add_vector.py
@@ -4,20 +4,20 @@ from prompt_toolkit.shortcuts import ProgressBar
 from biblemate import config, BIBLEMATEDATA
 
 
-def add_vector_dictionaries():
-    db_file = os.path.join(BIBLEMATEDATA, "data", "dictionary.data")
+def add_vector_data(db_file="dictionary.data", table="Dictionary"):
+    db_file = os.path.join(BIBLEMATEDATA, "data", db_file)
     if os.path.isfile(db_file):
         with apsw.Connection(db_file) as connection:
             cursor = connection.cursor()
             # Check if 'entry' column already exists
-            cursor.execute("PRAGMA table_info(Dictionary);")
+            cursor.execute(f"PRAGMA table_info({table});")
             columns = cursor.fetchall()
             column_names = [col[1] for col in columns]
             if 'entry' not in column_names:
-                cursor.execute("ALTER TABLE Dictionary ADD COLUMN entry TEXT;")
-                cursor.execute("ALTER TABLE Dictionary ADD COLUMN entry_vector TEXT;")
+                cursor.execute(f"ALTER TABLE {table} ADD COLUMN entry TEXT;")
+                cursor.execute(f"ALTER TABLE {table} ADD COLUMN entry_vector TEXT;")
             # Update 'entry' and 'entry_vector' columns
-            cursor.execute("SELECT path, content FROM Dictionary;")
+            cursor.execute(f"SELECT path, content FROM {table};")
             with ProgressBar() as pb:
                 for path, content in pb(cursor.fetchall()):
                     search = re.search(">([^<>]+?)</ref>", content)
@@ -25,9 +25,9 @@ def add_vector_dictionaries():
                         entry = search.group(1)
                         vector = get_embeddings([entry], config.embedding_model)
                         vector_str = json.dumps(vector.tolist())
-                        cursor.execute("UPDATE Dictionary SET entry = ?, entry_vector = ? WHERE path = ?;", (entry, vector_str, path))
-            cursor.execute(f"ALTER TABLE Dictionary DROP COLUMN content;")
-            cursor.execute(f"VACUU;")
+                        cursor.execute(f"UPDATE {table} SET entry = ?, entry_vector = ? WHERE path = ?;", (entry, vector_str, path))
+            cursor.execute(f"ALTER TABLE {table} DROP COLUMN content;")
+            cursor.execute(f"VACUUM;")
 
 def add_vector_encyclopedias():
     db_file = os.path.join(BIBLEMATEDATA, "data", "encyclopedia.data")
@@ -54,4 +54,4 @@ def add_vector_encyclopedias():
                             vector_str = json.dumps(vector.tolist())
                             cursor.execute(f"UPDATE {table} SET entry = ?, entry_vector = ? WHERE path = ?;", (entry, vector_str, path))
                 cursor.execute(f"ALTER TABLE {table} DROP COLUMN content;")
-            cursor.execute(f"VACUU;")
+            cursor.execute(f"VACUUM;")
