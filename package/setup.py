001423031b5b5fdaf505a57e2a2c8c31fe050f07
eliranwong <hkwebtech@gmail.com>
2025-10-21 21:54:16 +0100

    use latest agentmake library

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools.command.install import install
 import os, shutil, platform, sys
 
-version = "0.1.97"
+version = "0.1.99"
 with open(os.path.join("biblemate", "version.txt"), "w", encoding="utf-8") as fileObj:
     fileObj.write(version)
 
